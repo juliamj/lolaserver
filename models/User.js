0a0313a4b7b4a5023df260f4ed779a573a3700cb
holdenmad <holden.madagame@gmail.com>
2020-08-25 14:17:28 +0200

    Edited User Mongoose model with our parameters

--- models/User.js
+++ models/User.js
@@ -1,24 +1,76 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    min: 8,
+    max: 30,
+    required: true,
+  },
+  location: {
+    type: String,
+    min: 8,
+    max: 100,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatarURL: {
+    type: String,
+    min: 8,
+    max: 100,
+    required: false,
+  },
+  birthday: {
+    type: Date,
+    required: false,
+  },
+  freetext: {
+    type: String,
+    min: 8,
+    max: 1000,
+    required: false,
+  },
+  nativelang: {
+    type: String,
+    min: 8,
+    max: 30,
+    required: true,
+  },
+  otherlangs: {
+    type: String,
+    min: 8,
+    max: 30,
+    required: false,
+  },
+  learnlangs: {
+    type: String,
+    min: 8,
+    max: 30,
+    required: true,
+  },
+  interests: {
+    type: String,
+    min: 8,
+    max: 30,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
