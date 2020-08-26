2647331dd9be5f80000858dd107729184bf62059
holdenmad <holden.madagame@gmail.com>
2020-08-26 13:34:05 +0200

    Create matching logic

---
+++ models/Match.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const MessageSchema = new mongoose.Schema({
+content: {
+    type: String,
+    required: true,
+},
+authorId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+},
+
+}, {timestamps: true})
+
+const MatchSchema = new mongoose.Schema({
+  initialMatchCriteria: {
+    type: Array,
+    required: true,
+  },
+  groupName: {
+    type: String,
+    required: false,
+  },
+  users: {
+      type: Array,
+      required: true,
+  },
+  messages: {
+    type: [MessageSchema],
+    required: true,
+},
+})
+
+const Match = mongoose.model("matches", MatchSchema);
+
+module.exports = Match
--- models/User.js
+++ models/User.js
@@ -9,60 +9,8 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  gender: {
-    type: String,
-    min: 8,
-    max: 30,
-    required: true,
-  },
-  location: {
-    type: String,
-    min: 8,
-    max: 100,
-    required: true,
-  },
   password: {
     type: String,
-    required: true,
-  },
-  avatarURL: {
-    type: String,
-    min: 8,
-    max: 100,
-    required: false,
-  },
-  birthday: {
-    type: Date,
-    required: false,
-  },
-  freetext: {
-    type: String,
-    min: 8,
-    max: 1000,
-    required: false,
-  },
-  nativelang: {
-    type: String,
-    min: 8,
-    max: 30,
-    required: true,
-  },
-  otherlangs: {
-    type: String,
-    min: 8,
-    max: 30,
-    required: false,
-  },
-  learnlangs: {
-    type: String,
-    min: 8,
-    max: 30,
-    required: true,
-  },
-  interests: {
-    type: String,
-    min: 8,
-    max: 30,
     required: true,
   },
   date: {
