0f422ef588933975246cbf5a3f448a76b1f696e4
holdenmad <holden.madagame@gmail.com>
2020-08-27 13:47:41 +0200

    Implementing access tokens

--- app.js
+++ app.js
@@ -22,29 +22,29 @@ app.set("view engine", "ejs");
 //Bodyparser Middlware
 app.use(express.urlencoded({ extended: false }));
 
-//Express Session
-app.use(
-  session({
-    secret: "secret",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
+// //Express Session
+// app.use(
+//   session({
+//     secret: "secret",
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
 
 //Passport Middleware
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 //Connect flash
-app.use(flash());
+// app.use(flash());
 
-// Global Variables
-app.use((req, res, next) => {
-  res.locals.success_msg = req.flash("success_msg");
-  res.locals.error_msg = req.flash("error_msg");
-  res.locals.error = req.flash("error");
-  next();
-});
+// // Global Variables
+// app.use((req, res, next) => {
+//   res.locals.success_msg = req.flash("success_msg");
+//   res.locals.error_msg = req.flash("error_msg");
+//   res.locals.error = req.flash("error");
+//   next();
+// });
 
 //Routes
 app.get("/", require("./routes/index"));
@@ -52,6 +52,7 @@ app.use("/users", require("./routes/users"));
 app.use("/dashboard", require("./routes/dashboard"));
 app.use("/matches", require("./routes/matches"));
 app.use("/profiles", require("./routes/profiles"));
+
 
 const PORT = process.env.PORT || 5000;
 
