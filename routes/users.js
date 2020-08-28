26722e3f757af83c912043a83912b587c8a7a2de
holdenmad <holden.madagame@gmail.com>
2020-08-28 14:00:39 +0200

    Commented out register handle for later

--- routes/users.js
+++ routes/users.js
@@ -47,77 +47,7 @@ router.get("/login", (req, res) => res.render("login"));
 // Register page
 router.get("/register", (req, res) => res.render("register"));
 
-// Register Handle
-router.post("/register", (req, res) => {
-  const { name, email, password, password2 } = req.body;
-  let errors = [];
 
-  // Check required fields
-  if (!name || !email || !password || !password2) {
-    errors.push({ msg: "Please fill in all fields" });
-  }
-
-  // Check if passwords match
-  if (password !== password2) {
-    errors.push({ msg: "Passwords do not match" });
-  }
-
-  // Check password length
-  if (password.length < 6) {
-    errors.push({ msg: "Password should be at least 6 characters" });
-  }
-
-  if (errors.length > 0) {
-    res.render("register", {
-      errors,
-      name,
-      email,
-      password,
-      password2,
-    });
-  } else {
-    // Validation passed
-    User.findOne({ email: email }).then((user) => {
-      if (user) {
-        //User exists
-        errors.push({ msg: "Email is already registered" });
-        res.render("register", {
-          errors,
-          name,
-          email,
-          password,
-          password2,
-        });
-      } else {
-        const newUser = new User({
-          name,
-          email,
-          password,
-        });
-
-        // Hash password
-        bcrypt.genSalt(10, (err, salt) =>
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            // Set password to hasehd
-            newUser.password = hash;
-            // Save user
-            newUser
-              .save()
-              .then((user) => {
-                req.flash(
-                  "success_msg",
-                  "You are now registered and can log in"
-                );
-                res.redirect("/users/login");
-              })
-              .catch((err) => console.log(err));
-          })
-        );
-      }
-    });
-  }
-});
 
 // Login Handle
 router.post("/login", async (req, res) => {
@@ -165,3 +95,75 @@ module.exports = router;
 //     req.flash('success_msg', 'You are logged out');
 //     res.redirect('/users/login');
 // })
+
+// // Register Handle
+// router.post("/register", (req, res) => {
+//   const { name, email, password, password2 } = req.body;
+//   let errors = [];
+
+//   // Check required fields
+//   if (!name || !email || !password || !password2) {
+//     errors.push({ msg: "Please fill in all fields" });
+//   }
+
+//   // Check if passwords match
+//   if (password !== password2) {
+//     errors.push({ msg: "Passwords do not match" });
+//   }
+
+//   // Check password length
+//   if (password.length < 6) {
+//     errors.push({ msg: "Password should be at least 6 characters" });
+//   }
+
+//   if (errors.length > 0) {
+//     res.render("register", {
+//       errors,
+//       name,
+//       email,
+//       password,
+//       password2,
+//     });
+//   } else {
+//     // Validation passed
+//     User.findOne({ email: email }).then((user) => {
+//       if (user) {
+//         //User exists
+//         errors.push({ msg: "Email is already registered" });
+//         res.render("register", {
+//           errors,
+//           name,
+//           email,
+//           password,
+//           password2,
+//         });
+//       } else {
+//         const newUser = new User({
+//           name,
+//           email,
+//           password,
+//         });
+
+//         // Hash password
+//         bcrypt.genSalt(10, (err, salt) =>
+//           bcrypt.hash(newUser.password, salt, (err, hash) => {
+//             if (err) throw err;
+//             // Set password to hasehd
+//             newUser.password = hash;
+//             // Save user
+//             newUser
+//               .save()
+//               .then((user) => {
+//                 req.flash(
+//                   "success_msg",
+//                   "You are now registered and can log in"
+//                 );
+//                 res.redirect("/users/login");
+//               })
+//               .catch((err) => console.log(err));
+//           })
+//         );
+//       }
+//     });
+//   }
+// });
