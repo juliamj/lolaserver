ec9b3f772c8e1562ccbf42bae5ba96ccfaaf74d5
Julia Jannsen <jannsen.julia@gmx.de>
2020-09-01 16:23:18 +0200

    cleanup serverside and save access token

--- app.js
+++ app.js
@@ -1,50 +1,17 @@
 const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const flash = require("connect-flash");
-const session = require("express-session");
-const passport = require("passport");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors({ origin: true, exposedHeaders: ["x-auth-token"]}))
 app.use(express.json())
-
-// Passport config
-require("./config/passport")(passport);
 
 //DB Config
 require("dotenv").config();
 require("./database/client");
 
-//EJS Middleware
-app.use(expressLayouts);
-app.set("view engine", "ejs");
-
 //Bodyparser Middlware
 app.use(express.urlencoded({ extended: false }));
-
-// //Express Session
-// app.use(
-//   session({
-//     secret: "secret",
-//     resave: true,
-//     saveUninitialized: true,
-//   })
-// );
-
-//Passport Middleware
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-//Connect flash
-// app.use(flash());
-
-// // Global Variables
-// app.use((req, res, next) => {
-//   res.locals.success_msg = req.flash("success_msg");
-//   res.locals.error_msg = req.flash("error_msg");
-//   res.locals.error = req.flash("error");
-//   next();
-// });
 
 //Routes
 app.get("/", require("./routes/index"));
@@ -52,7 +19,6 @@ app.use("/users", require("./routes/users"));
 app.use("/dashboard", require("./routes/dashboard"));
 app.use("/matches", require("./routes/matches"));
 app.use("/profiles", require("./routes/profiles"));
-
 
 const PORT = process.env.PORT || 5000;
 
--- config/auth2.js
+++ config/auth2.js
@@ -4,11 +4,12 @@ module.exports = function (req, res, next) {
   //get the token from the header if present
   console.log(req.headers);
   let token = req.headers["x-access-token"] || req.headers["authorization"];
-  const tokenParts = token.split(" "); //allows us to get just the token without 'Bearer'
-  token = tokenParts[tokenParts.length - 1];
   //if no token found, return response (without going to the next middelware)
   if (!token) return res.status(401).send("Access denied. No token provided.");
-
+  console.log(token);
+  const tokenParts = token.split(" "); //allows us to get just the token without 'Bearer'
+  token = tokenParts[tokenParts.length - 1];
+  
   try {
     //if can verify the token, set req.user and pass to next middleware
     console.log(process.env.SECRET);
