062af766697a5dfd23bf438d69aa0cb581273687
holdenmad <holden.madagame@gmail.com>
2020-09-09 10:11:46 +0200

    Changed index.js to res.send('welcome')

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 //Welcome Page
-router.get('/', (req, res) => res.render('welcome'));
+router.get('/', (req, res) => res.send('welcome'));
 
 module.exports = router;
